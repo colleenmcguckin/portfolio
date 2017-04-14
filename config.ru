a4c2530fc64fb8cf60889a2420f613a1bf49e2f4
colleenmcguckin <colleenmcguckin@gmail.com>
2017-04-14 15:20:48 -0700

    set up contact form

--- config.ru
+++ config.ru
@@ -4,6 +4,7 @@ require 'rack/unreloader'
 Unreloader = Rack::Unreloader.new{Portfolio}
 
 require 'roda'
+require 'forme/erb'
 
 Unreloader.require './portfolio.rb'
 run Unreloader
--- portfolio.rb
+++ portfolio.rb
@@ -6,7 +6,7 @@ class Portfolio < Roda
   plugin :head
   plugin :assets, css: 'style.css'
   plugin :static, ['/images'], root: 'assets'
-  plugin :render, engine: 'haml' 
+  plugin :render, engine: 'haml'
   plugin :public, root: 'public/images'
 
   route do |r|
