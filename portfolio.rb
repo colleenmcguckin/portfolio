a063eeef9d7061b52ff170256f85c00f62871cec
colleenmcguckin <colleenmcguckin@gmail.com>
2017-04-14 16:02:38 -0700

    capture params

--- portfolio.rb
+++ portfolio.rb
@@ -8,6 +8,8 @@ class Portfolio < Roda
   plugin :static, ['/images'], root: 'assets'
   plugin :render, engine: 'haml'
   plugin :public, root: 'public/images'
+  plugin :mailer
+  plugin :params_capturing
 
   route do |r|
     r.public
@@ -24,6 +26,22 @@ class Portfolio < Roda
     r.get 'work' do
       @active = 'work'
       view("work")
+    end
+    r.on ['send_mail', :name, :email, :message] do
+      puts r[:name]
+      puts r[:email]
+      puts r[:message]
+
+      r.mail 'contact_inquiry' do
+        @name = r[:name]
+        @email = r[:email]
+        @message = r[:message]
+        from 'colleenmcguckin@gmail.com'
+        to 'colleenmcguckin@gmail.com'
+        subject "New Contact Inquiry from #{@name}"
+        render :contact_inquiry_email
+      end
+      r.redirect "/contact"
     end
   end
 end
